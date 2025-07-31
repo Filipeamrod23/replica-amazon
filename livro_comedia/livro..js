65aad8e946ae4b07f20c9449b4e5b4b017f8a2f1
Filipeamrod23 <filipeamrod@gmail.com>
2025-07-31 19:20:04 -0400

    Update livros html, css e js

---
+++ livro_comedia/livro..js
@@ -0,0 +1,6 @@
+let modalShow = document.querySelector('#buy-button');
+let modal = document.querySelector('.modal')
+
+modalShow.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
