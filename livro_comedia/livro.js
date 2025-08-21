aba019cd2ebc0b16ce63859e1334a37f72c5e1f7
Filipeamrod23 <filipeamrod@gmail.com>
2025-08-21 19:11:29 -0400

    Update livros e mais

---
+++ livro_comedia/livro.js
@@ -0,0 +1,88 @@
+let modalShow = document.querySelector("#buy-button");
+let modal = document.querySelector(".modal");
+let modalInput = document.querySelector(".modalInput");
+
+modalShow.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+let modalClose = document.querySelector(".button-close", function () {});
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+let searchButton = document.querySelector("#searchButton");
+
+async function enviarDados() {
+  let nome = document.getElementById("input-nome").value.trim();
+  let email = document.getElementById("input-email").value.trim();
+
+  //
+  if (!nome || !email) {
+    alert("Preencha todos os campos");
+  }
+
+  try {
+    // a API ela é especial, ela é um recurso assincrono, eu não sei onde está minha API, e para acessar a API, eu preciso de uma função especial
+
+    let resp = await fetch("http://localhost:3000/enviar-dados", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ nome, email }),
+    });
+
+    if (resp.ok) {
+      alert("Dados enviados com sucesso");
+    } else {
+      alert("Erro ao enviar dados");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+searchButton.addEventListener("click", async function () {
+  let cep = modalInput.value.trim();
+  let cepCalc = document.querySelector(".calc-result");
+
+  if (!cep) {
+    cepCalc.innerHTML = `<p>Por favor, insira um CEP</p>`;
+    cepCalc.style.color = "red";
+    return;
+  }
+
+  cepCalc.style.color = "";
+
+  if (cep.length !== 8) {
+    cepCalc.innerHTML = `<p>Por favor, inisira um CEP válido (8 dígitos)</p>`;
+    cepCalc.style.color = "red";
+    return;
+  }
+  cepCalc.style.color = "";
+
+  try {
+    let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    let data = await response.json();
+
+    if (data.erro) {
+      cepCalc.innerHTML = `<p>CEP não encontrado</p>`;
+      cepCalc.style.color = "red";
+    } else {
+      cepCalc.innerHTML = `
+      <p>Endereço: ${data.logradouro}</p>
+      <p>Bairro: ${data.bairro}</p>
+      <p>Cidade: ${data.localidade} - ${data.uf}</p>
+      <input class="input-api" id="input-nome" placeholder = "Digite seu nome"></input>
+      <input class="input-api" id="input-email" placeholder = "Digite seu email"></input>
+      <button id="button-api">Enviar</button>
+      `;
+      document
+        .querySelector("#button-api")
+        .addEventListener("click", enviarDados);
+    }
+  } catch (error) {
+    cepCalc.innerHTML = `<p>Erro: ${error}</p>`;
+    cepCalc.style.color = "red";
+  }
+});
--- server/index.js
+++ server/index.js
@@ -1,34 +1,66 @@
-
 // CRUD (CREATE)(POST), (READ)(GET), (UPDATE)(PUT), (DELETE)(DELETE)
 // FUNÇÕES DO BACKEND: (GET), (POST), (PUT), (DELETE)
 // ROTAS, https://example.net (HOME)
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require('cors')
+const app = express();
 const port = 3000;
 
 // banco de dados mocado
-const users = [
-    {id: 1, nome: "Tábata", email: "devinec465@discrip.com"},
-    {id: 2, nome: "Filipe", email: "2347865@discrip.com"}
-]
+const users = [];
 
-app.use(express.json())
+app.use(cors())
+
+app.use(express.json());
 // localhost:3000
-app.get('/usuarios', (req, res)=>{
-    res.json(users)
+app.get("/usuarios", (req, res) => {
+  res.json(users);
 });
 
-app.get('/usuarios/:id', (req, res)=>{
-    const id = parseInt(req.params.id);
+app.get("/usuarios/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const usuario = users.find(u => u.id == id)
-    if(!usuario){
-        return res.status(404).json({mensagem: 'usuário não foi encontrado'})
-    }
-    res.json(usuario);
-})
+  const usuario = users.find((u) => u.id == id);
+  if (!usuario) {
+    return res.status(404).json({ mensagem: "usuário não foi encontrado" });
+  }
+  res.json(usuario);
+});
 
-app.listen(port, ()=>{
-    console.log('porta está sendo escutada no endereço localhost')
-})
+app.post('/enviar-dados', (req, res) => {
+    const {nome, email} = req.body;
+    const novoUsuario = {
+        id: users.length + 1,
+        nome,
+        email
+    } 
+    // colocar alerta para quando a informação chegar no backend avisar o usuário
+    users.push(novoUsuario);
+
+
+    //200: informação
+    // 201: informação criada com sucesso
+    console.log("Novo usuário recebido: ", novoUsuario);
+
+    res.status(201).json({
+      mensagem: 'Dados recebidos com sucesso'
+    })
+
+
+});
+
+app.put('/usuarios/:id', (req, res)=>{
+  // A informação ID vem da URL Acessada através do frontend.
+  const id = parseInt(req.params.id)
+  // A informação nome e e-mail vem através do body porque ela precisa ser "escondida"
+  const {nome, email} = req.body;
+
+  const usuario = users.find(u => u.id === id)
+});
+
+app.delete('/usuarios/:id', (req, res)=>{});
+
+app.listen(port, () => {
+  console.log("porta está sendo escutada no endereço localhost");
+});
